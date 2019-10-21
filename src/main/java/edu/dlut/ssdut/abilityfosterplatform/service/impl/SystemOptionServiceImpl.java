d2be361b1126d026ba35dc0314b340a03a6b45d6
LHD <1789111694@qq.com>
2019-10-21 15:22:49 +0800

    #201910211522-systemoption后台接口

---
+++ src/main/java/edu/dlut/ssdut/abilityfosterplatform/service/impl/SystemOptionServiceImpl.java
@@ -0,0 +1,44 @@
+package edu.dlut.ssdut.abilityfosterplatform.service.impl;
+
+import edu.dlut.ssdut.abilityfosterplatform.mapper.SystemOptionMapper;
+import edu.dlut.ssdut.abilityfosterplatform.model.SystemOption;
+import edu.dlut.ssdut.abilityfosterplatform.repository.SystemOptionRepository;
+import edu.dlut.ssdut.abilityfosterplatform.service.SystemOptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author: lhd
+ * @Date 2019/10/17
+ * @Description:
+ **/
+
+@Service
+public class SystemOptionServiceImpl implements SystemOptionService{
+    @Autowired
+    private SystemOptionMapper systemOptionMapper;
+    @Autowired
+    private SystemOptionRepository systemOptionRepository;
+
+    @Override
+    public int insert(SystemOption record) {
+        systemOptionMapper.insert(record);
+        return 0;
+    }
+
+    @Override
+    public int insertSelective(SystemOption record) {
+        systemOptionMapper.insertSelective(record);
+        return 0;
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(SystemOption record) {
+        return systemOptionMapper.updateByPrimaryKeySelective(record);
+    }
+}
