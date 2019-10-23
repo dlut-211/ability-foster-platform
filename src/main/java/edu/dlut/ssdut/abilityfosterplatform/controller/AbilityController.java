212785281d74db63d0507481023434d22e24a2f2
zhuJiangKaiGG <1213503281@qq.com>
2019-10-23 10:29:59 +0800

    zjk20191023

--- src/main/java/edu/dlut/ssdut/abilityfosterplatform/controller/AbilityController.java
+++ src/main/java/edu/dlut/ssdut/abilityfosterplatform/controller/AbilityController.java
@@ -1,5 +1,8 @@
 package edu.dlut.ssdut.abilityfosterplatform.controller;
 
+import edu.dlut.ssdut.abilityfosterplatform.dto.StudentAbilityNameAndMaxDTO;
+import edu.dlut.ssdut.abilityfosterplatform.dto.SubjectTestAbilityScoreDTO;
+import edu.dlut.ssdut.abilityfosterplatform.dto.SubjectWorkAbilityScoreDTO;
 import edu.dlut.ssdut.abilityfosterplatform.enums.ResultEnum;
 import edu.dlut.ssdut.abilityfosterplatform.model.Ability;
 import edu.dlut.ssdut.abilityfosterplatform.model.SystemOption;
@@ -15,7 +18,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.*;
 
 /**
  * @Author: lhd
@@ -67,4 +70,75 @@ public class AbilityController {
         abilityRepository.deleteById(id);
         return ResultVOUtil.success();
     }
+    @ApiOperation("能力点图")
+    @RequestMapping("/gettestabilitydata")
+    public ResultVO getAbilityData(Integer subjectId,Integer studentId){
+        List<SubjectTestAbilityScoreDTO> testAbilityList = abilityService.ProceduregetSubjectTestAbilityScore(subjectId, studentId);
+        List<SubjectWorkAbilityScoreDTO> workAbilityList = abilityService.ProceduregetSubjectWorkAbilityScore(subjectId, studentId);
+        List<StudentAbilityNameAndMaxDTO> studentAbilityNameAndMaxDTOS = new ArrayList<StudentAbilityNameAndMaxDTO>();
+        List<StudentAbilityNameAndMaxDTO> realStudentAbilityNameAndMaxDTOS= new ArrayList<StudentAbilityNameAndMaxDTO>();
+        if(testAbilityList!=null&& workAbilityList!=null){
+            for (int i = 0; i < testAbilityList.size(); i++)
+            {
+                if (testAbilityList.get(i).getAbilityname()!=null) {
+                    StudentAbilityNameAndMaxDTO mode = new StudentAbilityNameAndMaxDTO();
+                    StudentAbilityNameAndMaxDTO realMode = new StudentAbilityNameAndMaxDTO();
+                    mode.setName(testAbilityList.get(i).getAbilityname());
+                    realMode.setName(testAbilityList.get(i).getAbilityname());
+                    if (String.valueOf(testAbilityList.get(i).getAbilityscoresum())==null){
+                        mode.setMax(0);
+                    }
+                    else{
+                        mode.setMax(testAbilityList.get(i).getAbilityscoresum());
+                    }
+                    if (String.valueOf(testAbilityList.get(i).getAbilityscore())==null){
+                        realMode.setMax(0);
+                    }
+                    else
+                        realMode.setMax(testAbilityList.get(i).getAbilityscore());
+                    studentAbilityNameAndMaxDTOS.add(mode);
+                    realStudentAbilityNameAndMaxDTOS.add(realMode);
+                }
+            }
+            for (SubjectWorkAbilityScoreDTO subjectWorkAbilityScoreDTO : workAbilityList) {
+                int flag=0;
+                int realFlag=0;
+                for (StudentAbilityNameAndMaxDTO item : studentAbilityNameAndMaxDTOS) {
+                    if (item.getName().equals(subjectWorkAbilityScoreDTO.getAbilityname())) {
+                        flag=1;
+                        item.setMax(item.getMax()+subjectWorkAbilityScoreDTO.getAbilityscoresum());
+                        break;
+                    }
+                }
+                for (StudentAbilityNameAndMaxDTO item : realStudentAbilityNameAndMaxDTOS) {
+                    if (item.getName().equals(subjectWorkAbilityScoreDTO.getAbilityname())) {
+                        realFlag=1;
+                        item.setMax(item.getMax()+subjectWorkAbilityScoreDTO.getAbilityscore());
+                    }
+                }
+                if (flag==0){
+                    StudentAbilityNameAndMaxDTO mode = new StudentAbilityNameAndMaxDTO();
+                    mode.setName(subjectWorkAbilityScoreDTO.getAbilityname());
+                    mode.setMax(subjectWorkAbilityScoreDTO.getAbilityscoresum());
+                    studentAbilityNameAndMaxDTOS.add(mode);
+                }
+                if (realFlag==0){
+                    StudentAbilityNameAndMaxDTO realMode = new StudentAbilityNameAndMaxDTO();
+                    realMode.setName(subjectWorkAbilityScoreDTO.getAbilityname());
+                    realMode.setMax(subjectWorkAbilityScoreDTO.getAbilityscore());
+                    realStudentAbilityNameAndMaxDTOS.add(realMode);
+                }
+            }
+        }
+        Map<String, Object> map = new HashMap<String,Object>();
+        map.put("studentAbilityNameAndMaxDTOS", studentAbilityNameAndMaxDTOS);
+        map.put("realStudentAbilityNameAndMaxDTOS", realStudentAbilityNameAndMaxDTOS);
+        return ResultVOUtil.success(map);
+    }
+
+//    @RequestMapping("/getworkabilitydata")
+//    public ResultVO getWorkAbilityData(Integer subjectId,Integer studentId){
+//        List<SubjectWorkAbilityScoreDTO> workAbilityList = abilityService.ProceduregetSubjectWorkAbilityScore(subjectId, studentId);
+//        return ResultVOUtil.success(workAbilityList);
+//    }
 }
--- src/main/java/edu/dlut/ssdut/abilityfosterplatform/service/AbilityService.java
+++ src/main/java/edu/dlut/ssdut/abilityfosterplatform/service/AbilityService.java
@@ -1,6 +1,10 @@
 package edu.dlut.ssdut.abilityfosterplatform.service;
 
+import edu.dlut.ssdut.abilityfosterplatform.dto.SubjectTestAbilityScoreDTO;
+import edu.dlut.ssdut.abilityfosterplatform.dto.SubjectWorkAbilityScoreDTO;
 import edu.dlut.ssdut.abilityfosterplatform.model.Ability;
+
+import java.util.List;
 
 /**
  * @Author: raymond
@@ -11,4 +15,8 @@ public interface AbilityService {
     Ability selectByPrimaryKey(Integer id);
 
     int updateByPrimaryKeySelective(Ability record);
+
+    List<SubjectTestAbilityScoreDTO> ProceduregetSubjectTestAbilityScore(Integer subjectId, Integer studentId);
+
+    List<SubjectWorkAbilityScoreDTO> ProceduregetSubjectWorkAbilityScore(Integer subjectId, Integer studentId);
 }
