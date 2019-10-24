09441adf29dcd5d7cf2cfbf737f2cbdd232850b2
Raymond Zhao <qq740567396@gmail.com>
2019-10-24 16:20:54 +0800

    完成知识点各接口_raymond

---
+++ src/main/java/edu/dlut/ssdut/abilityfosterplatform/dto/KnowledgeAbilityDTO.java
@@ -0,0 +1,9 @@
+package edu.dlut.ssdut.abilityfosterplatform.dto;
+
+/**
+ * @Author: raymond
+ * @Date: 2019/10/23 21:12
+ * @Description:
+ **/
+public class KnowledgeAbilityDTO {
+}
---
+++ src/main/java/edu/dlut/ssdut/abilityfosterplatform/service/impl/KnowledgeServiceImpl.java
@@ -0,0 +1,9 @@
+package edu.dlut.ssdut.abilityfosterplatform.service.impl;
+
+/**
+ * @Author: raymond
+ * @Date: 2019/10/23 21:12
+ * @Description:
+ **/
+public class KnowledgeServiceImpl {
+}
