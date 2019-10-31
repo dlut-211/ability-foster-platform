9b0586f1cd26ca7e19bbbe67c9c435340168dad2
wdz <123@163.com>
2019-10-31 15:34:51 +0800

    学生章节作业分页修改——wdz

--- src/main/java/edu/dlut/ssdut/abilityfosterplatform/mapper/VChapterWorkZjkMapper.java
+++ src/main/java/edu/dlut/ssdut/abilityfosterplatform/mapper/VChapterWorkZjkMapper.java
@@ -4,11 +4,13 @@ import edu.dlut.ssdut.abilityfosterplatform.model.VChapterWorkZjk;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface VChapterWorkZjkMapper {
     int insert(VChapterWorkZjk record);
 
     int insertSelective(VChapterWorkZjk record);
 
     //学生页面—我的课堂—章节作业
-    Page<VChapterWorkZjk> VChapterWorkPage(Integer classroomId, Integer studentId, Pageable pageable);
+    List<VChapterWorkZjk> findChapterWork(Integer classroomId, Integer studentId, Integer startSize, Integer pageSize);
 }
--- src/main/java/edu/dlut/ssdut/abilityfosterplatform/service/VChapterWorkService.java
+++ src/main/java/edu/dlut/ssdut/abilityfosterplatform/service/VChapterWorkService.java
@@ -5,10 +5,12 @@ import edu.dlut.ssdut.abilityfosterplatform.model.VStudentWorkChapter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface VChapterWorkService {
 
     //学生页面—我的课堂—章节作业
-    Page<VChapterWorkZjk> VChapterWorkPage(Integer classroomId, Integer studentId, Pageable pageable);
+    List<VChapterWorkZjk> VChapterWorkPage(Integer classroomId, Integer studentId, Integer startSize, Integer pageSize);
     //学生页面——查看作业
     Page<VStudentWorkChapter> VStudentWorkChapterPage(Integer studentId,
                                                       Integer chapterId,
